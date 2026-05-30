b28b48bd2847fa423681b831eba454cb5b6124e9
26Mukesh <smukeshkumar240@gmail.com>
2026-05-30 11:13:11 +0530

    add docker

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from "express";
 import productRoutes from "./routes/product.routes.js";
+
 const app = express();
 app.use(express.json());
+
 app.use("/api/products", productRoutes);
+
 export default app;
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getProducts, createProduct } from "../controllers/product.controller.js";
+import { getProducts } from "../controllers/product.controller.js";
 const router = Router();
 
 router.get("/", getProducts);
